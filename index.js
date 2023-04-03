c396a3f252968a9f80f6f009fd5091c7b26e89d6
Max <maxmwangikenya@gmail.com>
2023-04-03 21:36:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  function writeCards(namesArray, occasion) {
+    let thankYouCards = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${occasion} gift!`;
+      thankYouCards.push(message);
+    }
+  
+    return thankYouCards;
+  }
+  
